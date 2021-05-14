cb077f957bd720cbd858eaf56c20dbf1784ed9df
leeminze214 <leeminz214@gmail.com>
2021-05-14 17:26:22 -0400

    ping

--- bott.py
+++ bott.py
@@ -3,11 +3,15 @@ from discord.ext import commands, tasks
  
 
 token = open('tok.txt').read()
-bot = commands.Bot(command_prefix='.',intents = discord.Intents.all(), help_command =None)
+bot = commands.Bot(command_prefix='.', help_command =None)
       
    
 @bot.event
 async def on_ready():
     print('logged in as {0.user}'.format(bot))
 
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Latency is {round(bot.latency*1000,2)}ms')
+    
 bot.run(token, bot = True)
