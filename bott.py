8b00427190871aeb92330a126ec3eec556b714a0
leeminze214 <leeminz214@gmail.com>
2021-05-14 20:13:05 -0400

    help function

--- bott.py
+++ bott.py
@@ -14,4 +14,23 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send(f'Pong! Latency is {round(bot.latency*1000,2)}ms')
     
+
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed()
+    embed.title = 'Help - List of Commands'   
+    embed.color = 206694
+    embed.add_field(
+                name="View info on events",
+                value="`.event`",
+                inline= False
+                )
+    embed.add_field(
+                name="Access helpful links",
+                value="`.link`",
+                inline = False
+                )
+    await ctx.send(embed=embed)
+
+
 bot.run(token, bot = True)
