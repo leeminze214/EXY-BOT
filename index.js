421bb82fd7cca955b4549b03f1caf61fc0389514
leeminze214 <leeminz214@gmail.com>
2022-03-13 22:25:29 -0400

    slash commands

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+     else if (commandName === 'server') {
+		await interaction.reply(`Create Date: ${interaction.guild.createdAt}\nServer name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	}
+     else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+	}
+});
+
 client.login(token).then(() => {
 	console.log('Successfully logged in!');
 }).catch((error) => {
